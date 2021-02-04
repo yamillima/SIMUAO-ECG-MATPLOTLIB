5516dc2ba6ff16a5afdab4d94adacd5bb854b683
SorenAcevedo <70923936+SorenAcevedo@users.noreply.github.com>
2021-02-04 15:18:10 -0500

    Update ecg_class.py

--- ecg_class.py
+++ ecg_class.py
@@ -40,28 +40,42 @@ class Ecg():
 			'Taquicardia ventricular': 17
 		}
 		t = np.linspace(0.0, 3.0 * (60 / self.bpm), int(3.0 * (60 / self.bpm) * 100.0))
-		return AnomFunc(self.n, (60 / self.bpm), t, anom['Fibrilación auricular'])
+		return AnomFunc(self.n, (60 / self.bpm), t, anom[anomalia]) , t
 
 	@staticmethod
 	def add_noise(signal, fre, t):
 		""" Método para añadir ruido """
 		return NoiseAdd(signal, fre, t)
 
+	@staticmethod
+	def show_ecg(signal, t):
+		""" Método para mostrar derivadas """
+		name = ['I','II','III', 'avR', 'avF', 'avL', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
+
+		fig = plt.figure(figsize=(10, 10))
+		for i in range(0, 12):
+			fig.tight_layout(pad=2.0)
+			ax = plt.subplot(3, 4, i + 1)
+			ax.plot(t, signal[i])
+			ax.set_xlabel('x')
+			ax.set_ylabel('y')
+			ax.set_title('Derivada:' + name[i])
+		plt.show()
+
+
 ########################################### Creación de objeto ####################################################
 
-mio = Ecg(90, 60, 180)
+########################################## ECG NORMAL #############################################################
+
+mio = Ecg(90, 60, 5)
 array, t = mio.derivadas()
+Ecg.show_ecg(array, t)
+
+######################################### ECG ANOMALÍA ############################################################
+
+array, t = mio.anomalias('Fibrilación auricular')
+Ecg.show_ecg(array, t)
+
+######################################### ECG CON RUIDO ###########################################################
 withnoise = Ecg.add_noise(array, [1, 60], t)
-
-name = ['I','II','III', 'avR', 'avF', 'avL', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
-
-fig = plt.figure(figsize=(10, 10))
-fig.tight_layout()
-fig.subplots_adjust(left=0.125, bottom=0.1, right=None, top=0.9, wspace=None, hspace=None)
-for i in range(0, 12):
-	ax = plt.subplot(3, 4, i + 1)
-	ax.plot(t, withnoise[i])
-	ax.set_xlabel('x')
-	ax.set_ylabel('y')
-	ax.set_title('Derivada:' + name[i])
-plt.show()
+Ecg.show_ecg(withnoise, t)
